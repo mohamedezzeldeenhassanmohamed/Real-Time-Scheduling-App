c1941401768c0da7a3aa285d41fd25cac6c7456e
Mohamed Ezz <mohamedezzthassan@gmail.com>
2024-11-20 19:02:24 +0200

    real time app

---
+++ Real-Time-Scheduling-App-master/app/src/main/java/com/mohamedrafat/real_timeschedulingapp/model/StaticPriorityTasks.kt
@@ -0,0 +1,6 @@
+package com.mohamedrafat.real_timeschedulingapp.model
+
+class StaticPriorityTasks(val numTask: Int,
+                          val period: Int,
+                          val processingTime: Int ) {
+}
