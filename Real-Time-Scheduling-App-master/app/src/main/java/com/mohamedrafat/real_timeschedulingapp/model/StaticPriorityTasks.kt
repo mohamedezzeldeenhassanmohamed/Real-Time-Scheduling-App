8f3182bebbcfb13bf4b5ef22092a94144099dea1
Mohamed Ezz <mohamedezzthassan@gmail.com>
2024-11-20 19:04:18 +0200

    Delete Real-Time-Scheduling-App-master directory

--- Real-Time-Scheduling-App-master/app/src/main/java/com/mohamedrafat/real_timeschedulingapp/model/StaticPriorityTasks.kt
+++
@@ -1,6 +0,0 @@
-package com.mohamedrafat.real_timeschedulingapp.model
-
-class StaticPriorityTasks(val numTask: Int,
-                          val period: Int,
-                          val processingTime: Int ) {
-}
--- Real-Time-Scheduling-App-master/app/src/main/java/com/mohamedrafat/real_timeschedulingapp/model/StaticTableTasks.kt
+++
@@ -1,8 +0,0 @@
-package com.mohamedrafat.real_timeschedulingapp.model
-
-class StaticTableTasks(val numTask: Int,
-                       val arrivalTime: Int,
-                       val executionTime: Int ,
-                       val startingDeadline:Int ) {
-
-}
